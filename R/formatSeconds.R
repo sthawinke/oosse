77ab87828a45357cb9fd059bbdbf50101a45ec98
stijn <stijn.hawinkel@psb.ugent.be>
2023-04-04 10:48:09 +0200

    Better formatting

--- R/formatSeconds.R
+++ R/formatSeconds.R
@@ -7,9 +7,8 @@ formatSeconds = function(seconds){
     minutes = seconds %/% 60
     hours = minutes %/% 60
     days = hours %/% 24
-    secondsLeft = seconds %% 60
-    paste0(if(days > 0) {paste(days, "days ")},
-           if(hours > 0) {paste(hours, "hours ")},
-           if(minutes > 0) {paste(minutes, "minutes and ")},
-           paste(secondsLeft, "seconds"))
+    paste0(if(days > 0) {paste(days, "days, ")},
+           if(hours > 0) {paste(hours%%24, "hours, ")},
+           if(minutes > 0) {paste(minutes%%60, "minutes and ")},
+           paste(seconds %% 60, "seconds"))
 }
