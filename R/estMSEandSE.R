90267818cdad7e913d7699ac8b813f5a724754e5
stijn <stijn.hawinkel@psb.ugent.be>
2023-04-05 15:15:18 +0200

    Including correlation estimation

--- R/estMSEandSE.R
+++ R/estMSEandSE.R
@@ -2,9 +2,10 @@
 #'
 #' @param y The vector of outcome values
 #' @param x The matrix of predictors
-#' @param predFun The function for fitting the predicition model
-#' @param evalPredFun The function for evaluating the prediction model
+#' @param fitFun The function for fitting the predicition model
+#' @param predFun The function for evaluating the prediction model
 #' @param methodMSE The method to estimate the MSE, either "CV" for cross-validation or "bootstrap" for .632 bootstrap
+#' @param methodCor The method to estimate the correlation between MSE and MST estimators, either "nonparametric" or "jackknife"
 #' @param nFolds The number of outer folds for cross-validation
 #' @param nInnerFolds The number of inner cross-validation folds
 #' @param cvReps The number of repeats for the cross-validation
@@ -15,10 +16,11 @@
 #' @export
 #'
 #' @examples
-estMSEandSE = function(y, x, predFun, evalPredFun, methodMSE = c("CV", "bootstrap"),
-                       nFolds = 10, nInnerFolds = nFolds - 1, cvReps = 200, nBootstraps = 200, nInnerBootstraps = 200, ...){
-    predFun = checkPredFun(predFun)
+estMSEandSE = function(y, x, fitFun, predFun, methodMSE = c("CV", "bootstrap"), methodCor = c("nonparametric", "jackknife"),
+                       nFolds = 10, nInnerFolds = nFolds - 1, cvReps = 200, nBootstraps = 200, nInnerBootstraps = 200, nBootstrapsCor = 50, ...){
+    fitFun = checkFitFun(fitFun)
     methodMSE = match.arg(methodMSE)
+    methodCor = match.arg(methodCor)
     if((n <- length(y)) != NROW(x)){
         stop("Number of observations in y and x must match!")
     }
@@ -31,14 +33,16 @@ estMSEandSE = function(y, x, predFun, evalPredFun, methodMSE = c("CV", "bootstra
     }
 
     #Predict time this will take
-    singleRunTime = system.time(evalPredFun(fullModel <- predFun(y, x, id = seq_len(n), ...), newx = x))
+    singleRunTime = system.time(predFun(fullModel <- fitFun(y, x, id = seq_len(n), ...), newx = x))
     cat("Fitting and evaluating the model once took", formatSeconds(singleRunTime), ".\nYou requested",
         switch(methodMSE,
                "CV" = paste0(cvReps, " repeats of ", nFolds, "-fold cross-validation"),
                "bootstrap" = paste(nBootstraps*nInnerBootstraps, ".632 bootstrap instances")),
     "with", nCores <- multicoreWorkers(), "cores, which is expected to last for\n",
-    formatSeconds(singleRunTime/nCores * switch(methodMSE, "CV" = cvReps*nFolds, "bootstraps" = nBootstraps)))
+    formatSeconds((switch(methodMSE, "CV" = cvReps*nFolds, "bootstraps" = nBootstraps) +
+                       switch(methodCor, "nonparametric" = nBootstrapsCor, "jackknife" = n))*singleRunTime/nCores))
 
+    #Estimate MSE and its standard error
     if(methodMSE == "CV"){
         #Nested cross-validation
         unFolds <- seq_len(nFolds);unFoldsIn <- seq_len(nInnerFolds) #Prepare the folds
@@ -47,15 +51,15 @@ estMSEandSE = function(y, x, predFun, evalPredFun, methodMSE = c("CV", "bootstra
             lapply(unFolds, function(uf){
                 #Outer loop
                 idTrain = folds!=uf
-                modTrain = predFun(y, x, idTrain)
-                predTest = evalPredFun(modTrain, x[!idTrain,])
+                modTrain = fitFun(y, x, idTrain)
+                predTest = predFun(modTrain, x[!idTrain,])
                 eOut = (predTest-y[!idTrain])^2
                 #Inner loop
                 inFolds = sample(rep(unFoldsIn, length.out = sum(idTrain)))
                 eIn =  lapply(unFoldsIn, function(inf){
                     idTrainIn = inFolds!=inf
-                    modTrain = predFun(y, x, idTrainIn)
-                    predTest = evalPredFun(modTrain, x[!idTrainIn])
+                    modTrain = fitFun(y, x, idTrainIn)
+                    predTest = predFun(modTrain, x[!idTrainIn])
                     (predTest-y[!idTrainIn])^2
                 })
                 #summary statistics
@@ -69,24 +73,21 @@ estMSEandSE = function(y, x, predFun, evalPredFun, methodMSE = c("CV", "bootstra
     } else if(methodMSE == "bootstrap") {
         bootReps = bplapply(seq_len(nBootstraps), function(br){
             id = sample(n, replace = TRUE)
-            modTrain = predFun(y, x, seq_along(y)) #Fit on full model
-            eOut = evalPredFun(modTrain, x[-id,]) #Out of sample prediction
-            eIn = evalPredFun(modTrain, x) #In sample prediction
-            ErrOutOfSample = mean((eOut-y[-id])^2) #Out of sample error
-            ErrInSample = mean((eIn-y)^2)#In sample error
-            #c("ErrOutOfSample" = ErrOutOfSample, "ErrInSample" = ErrInSample)
-            #Inner bootstrap
-            oobInner = lapply(integer(nInnerBootstraps), function(ii){
-                idIn = sample(id, replace = TRUE)
-                bootOob(y, x, idIn, id)
-            })
-            bootObjOOBests = processOob(oobInner)
-            bootObj632ests =  process632In(bootObj632$obsBoot, oobObj = bootObjOOBests$procOb)
+            #.632 bootstrap
+            MSE632est = boot632(y, x, id, fitFun, predFun)
+            #Out of bag bootstrap
+            oob = bootOob(y, x, id, seq_along(y), fitFun, predFun)
+            list("oobObj" = oob, "MSE632est" = MSE632est)
         })
+        MSE632est = mean(vapply(FUN.VALUE = double(1), bootReps, function(x) {x$MSE632est}))
+        MSEoob = processOob(bootReps)
+        SEmse = MSE632est/MSEoob["MSEhat"]*MSEoob["SEhat"]
+        seVec = c("MSEhat" = MSE632est, "SEhat" = SEmse)
     }
+    corMSEMST = estCorMSEMST(y, x, fitFun, predFun, methodMSE, methodCor, nBootstrapsCor)
 }
 n = 40;p=3
 y = rnorm(n)
 x = matrix(rnorm(n*p),n,p)
-predFun = function(y, x, id){lm.fit(y = y[id], x = cbind(1, x[id,]))}
-evalPredFun = function(mod, x) {cbind(1,x) %*% mod$coef}
+fitFun = function(y, x, id){lm.fit(y = y[id], x = cbind(1, x[id,]))}
+predFun = function(mod, x) {cbind(1,x) %*% mod$coef}
