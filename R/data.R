4a5e87e6f93c7c78dd572a2ba5b3e55a3134854c
stijn <stijn.hawinkel@psb.ugent.be>
2023-04-06 13:41:55 +0200

    With brassica data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Gene expression and phenotypes of Brassica napus (rapeseed) plants
+#'
+#' RNA-sequencing data of genetically identical Brassica napus plants in autumn, with 5 phenotypes next spring
+#'
+#' @format Two dataframes: BrassicaExpr and BrassicaPheno
+#' \describe{
+#'   \item{BrassicaExpr}{Rlog values of 1000 most expressed genes}
+#'   \item{BrassicaPhenot}{Data frame with 5 phenotypes and x and y coordinates of the plants in the field}
+#' }
+#' @source \url{https://doi.org/10.1101/2022.10.21.513275 }
+"Brassica"
