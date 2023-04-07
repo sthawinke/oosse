1b855503a6b9ba01485c48506fcaeaf9cb254d4f
stijn <stijn.hawinkel@psb.ugent.be>
2023-04-07 10:08:28 +0200

    Remove comments

--- R/getSEsNested.R
+++ R/getSEsNested.R
@@ -10,8 +10,6 @@ getSEsNested = function(cvSplitReps, nOuterFolds, n){
     ErrNCV = mean(vapply(cvSplitReps, FUN.VALUE = double(nOuterFolds),
                          function(y) vapply(y, FUN.VALUE = double(1), function(x) x[["errHatTilde"]])))
     MSEhat = mean(vapply(cvSplitReps, FUN.VALUE = double(nOuterFolds), function(y) vapply(y, FUN.VALUE = double(1),function(x) x[["a"]]-x[["b"]])))
-    #mean(vapply(y, FUN.VALUE = double(nOuterFolds), cvSplitReps, function(y) vapply(y, FUN.VALUE = double(1),function(x) x[["a"]]))) -
-     #   mean(vapply(y, FUN.VALUE = double(nOuterFolds), function(y) vapply(y, FUN.VALUE = double(1),function(x) x[["b"]])))
     errOuter0 = lapply(cvSplitReps, function(y) lapply(y, function(x) x[["eOut"]]))
     mseOuter = vapply(FUN.VALUE = double(nOuterFolds), errOuter0, function(w) vapply(FUN.VALUE = double(1), w, mean))
     errOuter = unlist(errOuter0)
