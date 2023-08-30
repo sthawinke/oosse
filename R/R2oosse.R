0a3060383149f928922b157160ffcc7f9b65943f
stijn <stijn.hawinkel@psb.ugent.be>
2023-08-30 14:48:45 +0200

    Trying to get it to work on windows

--- R/R2oosse.R
+++ R/R2oosse.R
@@ -26,10 +26,12 @@
 #' @importFrom methods formalArgs
 #' @importFrom stats cor sd var
 #' @importFrom BiocParallel bplapply multicoreWorkers
+#' @importFrom doParallel registerDoParallel
+#' @importFrom Rdpack reprompt
 #'
 #' @details Implements the calculation of the R² and its standard error by \insertCite{Hawinkel2023}{oosse}.
-#'  Multithreading is used as provided by the BiocParallel package,
-#' A rough estimate of expected computation time is printed when prinTimeEstimate is true, but this is purely indicative.
+#' Multithreading is used as provided by the BiocParallel or doParallel packages,
+#' A rough estimate of expected computation time is printed when printTimeEstimate is true, but this is purely indicative.
 #' The options to estimate the mean squared error (MSE) are cross-validation \insertCite{Bates2021}{oosse} or the .632 bootstrap \insertCite{Efron1997}{oosse}.
 #' @examples
 #' data(Brassica)
@@ -40,7 +42,6 @@
 #' R2lm = R2oosse(y = Brassica$Pheno$Leaf_8_width, x = Brassica$Expr[, 1:10],
 #' fitFun = fitFunLM, predFun = predFunLM, nFolds = 10)
 #' @seealso \link{buildConfInt}
-#' @importFrom Rdpack reprompt
 #' @references
 #'   \insertAllCited{}
 R2oosse = function(y, x, fitFun, predFun, methodMSE = c("CV", "bootstrap"), methodCor = c("nonparametric", "jackknife"), printTimeEstimate = TRUE,
@@ -76,7 +77,7 @@ R2oosse = function(y, x, fitFun, predFun, methodMSE = c("CV", "bootstrap"), meth
                    "bootstrap" = paste(nBootstraps, ".632 bootstrap instances")),
         " with ", nCores <- multicoreWorkers(), " cores, which is expected to last for roughly\n",
         formatSeconds(sec <- (estMSEreps + estCorReps)*singleRunTime/nCores),
-        if(nCores==1 && (sec >10)) "\nConsider using multithreading with the 'BiocParallel' package to speed up computations.")
+        if(nCores==1 && (sec >10)) {"\nConsider using multithreading with the 'BiocParallel' package to speed up computations."}, "\n")
     }
     seVec = estMSE(y, x, fitFun, predFun, methodMSE, nFolds = nFolds, nInnerFolds = nInnerFolds, cvReps = cvReps, nBootstraps = nBootstraps)
     corMSEMST = estCorMSEMST(y, x, fitFun, predFun, methodMSE, methodCor, nBootstrapsCor, nFolds = nFolds, nBootstraps = nBootstraps)
