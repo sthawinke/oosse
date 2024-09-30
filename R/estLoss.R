a8543ac815889dd43abdd3b0b2ef5483bf4fdef7
stijn <stijn.hawinkel@psb.ugent.be>
2024-09-30 14:02:16 +0200

    bug in estloss

--- R/estLoss.R
+++ R/estLoss.R
@@ -2,11 +2,11 @@
 #'
 #' @param obs,pred Observed and predicted observations
 #' @param loss The loss type. For "squared" this is (obs-pred)^2, for
-#' "binary" it is I(obs=pred).
+#' "binary" it is 1-I(obs=pred).
 #'
 #' @return A vector of losses of the same length as obs and pred
 estLoss = function(obs, pred, loss){
     switch(loss,
            "squared" = (obs-pred)^2,
-           "binary" = obs == pred)
+           "binary" = obs != pred)
 }
